f415648329ec4c4d7e1df84e068d4d4d953113d1
AlinaGoor <41553929+AlinaGoor@users.noreply.github.com>
2019-05-12 22:46:20 +0200

    SecondSIweek

---
+++ SeconSIweek/InputValidator/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InputValidator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Regex nameRegex = new Regex(@"^[\p{L} \.\-]+$");
+            //Regex phoneRegex = new Regex(@"?([0-9]{3})?([0−9]3)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})");
+            Regex emailRegex = new Regex("(?<user>[^@]+)@(?<host>.+)");
+            Regex phoneRegex = new Regex(@"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$");
+            InformationValidation.ValidateName(txtName, nameRegex);
+            InformationValidation.ValidatePhone(txtPhone, phoneRegex);
+            InformationValidation.ValidateEmail(txtEmail, emailRegex);
+            string formattedPhone = InformationValidation.ReformatPhone(txtPhone.Text);
+            txtPhone.Text = formattedPhone;
+
+
+        }
+    }
+
+    class InformationValidation
+    {
+        public static void ValidateName(TextBox txtName, Regex nameRegex)
+        {
+            if (!Regex.IsMatch(txtName.Text, nameRegex.ToString()))
+            {
+                MessageBox.Show("The name is invalid.");
+            }
+
+        }
+
+        public static void ValidatePhone(TextBox txtPhone, Regex phoneRegex)
+        {
+            if (!Regex.IsMatch(txtPhone.Text, phoneRegex.ToString()))
+            {
+                MessageBox.Show("The phone number is invalid.");
+            }
+        }
+
+        public static void ValidateEmail(TextBox txtEmail, Regex emailRegex)
+        {
+            if (!Regex.IsMatch(txtEmail.Text, emailRegex.ToString()))
+            {
+                MessageBox.Show("Invalid E-mail address.");
+            }
+        }
+
+        public static string ReformatPhone(string phoneNum)
+        {
+            string result = string.Format("{0:(###) ###-###}", phoneNum);
+            return result;
+        }
+    }
+}
---
+++ SeconSIweek/SeekAndArchive/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = args[0];
+            string directoryName = args[1];
+            Search.RecursiveFileSearch(fileName, directoryName);
+        }
+    }
+
+    class Search
+    {
+        internal static void RecursiveFileSearch(string fileName, string directoryName)
+        {
+            DirectoryInfo di = new DirectoryInfo(directoryName);
+            WalkdirectoryTree(di, fileName);
+            Console.WriteLine("Press any key.");
+            Console.ReadKey();
+
+        }
+
+        internal static void WalkdirectoryTree(DirectoryInfo rootDir, string fileName)
+        {
+            FileInfo[] files = null;
+            try
+            {
+                files = rootDir.GetFiles(fileName);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            if (files != null)
+            {
+                foreach (var fileInfo in files)
+                {
+                    Console.WriteLine("\n The file  path of the requested file(s): {0}", fileInfo.FullName);
+                }
+
+                foreach (var subDir in rootDir.GetDirectories())
+                {
+                    WalkdirectoryTree(subDir, fileName);
+                }
+            }
+        }
+    }
+}
